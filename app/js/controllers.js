6deb71bd978cdf3069c063d0c8e118f499cb7871
unknown <slamplug@LTPW7SLAMPLUG2.corp.capgemini.com>
2014-09-10 14:41:19 +0100

    create intellij project and add addItem and removeItem basics

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,19 +5,36 @@
 var dtxControllers = angular.module('dtxControllers', []);
 
 dtxControllers.controller('PeriodSummaryCtrl', ['$scope', 'PeriodSummary',
-  function($scope, PeriodSummary) {
-    $scope.periodItems = PeriodSummary.query();
-    $scope.orderProp = 'id';
-  }]);
+    function ($scope, PeriodSummary) {
+        $scope.periodItems = PeriodSummary.query();
+        $scope.orderProp = 'id';
+
+        $scope.addItem = function () {
+            var len = $scope.periodItems.length + 1;
+            $scope.periodItems.push({ 'id': len, 'categoryName': '', 'projectCode': '', 'taskId': '', 'quantity': 0 });
+        };
+        $scope.removeItem = function (id) {
+            //alert( "remove item " + id );
+            var index = -1;
+            var comArr = eval( $scope.periodItems );
+            for( var i = 0; i < comArr.length; i++ ) {
+                if( comArr[i].id === id ) {
+                    index = i;
+                    break;
+                }
+            }
+            $scope.periodItems.splice( index, 1 );
+        };
+    }]);
 
 /*phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
-  function($scope, $routeParams, Phone) {
-    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-      $scope.mainImageUrl = phone.images[0];
-    });
+ function($scope, $routeParams, Phone) {
+ $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+ $scope.mainImageUrl = phone.images[0];
+ });
 
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
-    }
-  }]);
-*/
+ $scope.setImage = function(imageUrl) {
+ $scope.mainImageUrl = imageUrl;
+ }
+ }]);
+ */
